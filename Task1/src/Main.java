ac6760aea8e4bf72c614669779eb8b6cbdbf1256
r1411 <roma00055@gmail.com>
2021-12-25 22:33:08 +0300

    Рефакторинг Main в первой задаче

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,10 +1,10 @@
 import combobjects.*;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -21,92 +21,25 @@ public class Main {
         System.out.println("Введите число k: ");
         int k = scan.nextInt();
 
-
-        try {
-            FileWriter task1Writer = new FileWriter("task1_1.txt");
-            KPermsWithReps obj1 = new KPermsWithReps(elements, k);
-            writeToFile(obj1, task1Writer);
-            while(obj1.hasNext()) {
-                obj1.next();
-                writeToFile(obj1, task1Writer);
-            }
-            task1Writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            FileWriter task1Writer = new FileWriter("task1_2.txt");
-            Permutations obj2 = new Permutations(elements);
-            writeToFile(obj2, task1Writer);
-            while(obj2.hasNext()) {
-                obj2.next();
-                writeToFile(obj2, task1Writer);
-            }
-            task1Writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            FileWriter task1Writer = new FileWriter("task1_3.txt");
-            KPerms obj3 = new KPerms(elements, k);
-            writeToFile(obj3, task1Writer);
-            while(obj3.hasNext()) {
-                obj3.next();
-                writeToFile(obj3, task1Writer);
-            }
-            task1Writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            FileWriter task1Writer = new FileWriter("task1_4.txt");
-            Powerset obj4 = new Powerset(elements);
-            writeToFile(obj4, task1Writer);
-            while(obj4.hasNext()) {
-                obj4.next();
-                writeToFile(obj4, task1Writer);
-            }
-            task1Writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            FileWriter task1Writer = new FileWriter("task1_5.txt");
-            Combinations obj5 = new Combinations(elements, k);
-            writeToFile(obj5, task1Writer);
-            while(obj5.hasNext()) {
-                obj5.next();
-                writeToFile(obj5, task1Writer);
-            }
-            task1Writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        List<List<Character>> repCombs = Combinatorics.repeatableCombinations(elements, k);
-        writeToFile(repCombs, "task1_6.txt");
+        writeAllObjectsToFile(new KPermsWithReps(elements, k), "task1_1.txt");
+        writeAllObjectsToFile(new Permutations(elements), "task1_2.txt");
+        writeAllObjectsToFile(new KPerms(elements, k), "task1_3.txt");
+        writeAllObjectsToFile(new Powerset(elements), "task1_4.txt");
+        writeAllObjectsToFile(new Combinations(elements, k), "task1_5.txt");
+        writeAllObjectsToFile(new CombinationsWithReps(elements, k), "task1_6.txt");
     }
 
-    private static <T> void writeToFile(List<List<T>> objects, String fileName) {
-        File file = new File(fileName);
-        List<String> lines = new ArrayList<>();
-        lines.add("Всего объектов: " + objects.size());
-        for(List<T> object : objects) {
-            lines.add(object.toString());
-        }
+    private static void writeAllObjectsToFile(CombObject obj, String filename) {
         try {
-            Files.write(file.toPath(), lines);
+            FileWriter writer = new FileWriter(filename);
+            writer.write(obj.getCurrentObj().toString() + System.lineSeparator());
+            while(obj.hasNext()) {
+                obj.next();
+                writer.write(obj.getCurrentObj().toString() + System.lineSeparator());
+            }
+            writer.close();
         } catch (IOException e) {
-            System.err.println("Ошибка записи файла:");
             e.printStackTrace();
         }
-    }
-
-    private static void writeToFile(CombObject obj, FileWriter writer) throws IOException {
-        writer.write(obj.getCurrentObj().toString() + System.lineSeparator());
     }
 }
